82dc4c2492e2da4986ffb572a902bb13e38c088a
John Moore <john.moore@nationalarchives.gov.uk>
2024-02-23 13:23:05 +0000

    return annotation page

--- wass/data.py
+++ wass/data.py
@@ -1,32 +1,45 @@
 import chromadb
+import requests
+from flask import request
+
 
 class Data:
     def __init__(self, ctx: object) -> None:
         self.annotation_limit = ctx.annotation_limit
-        client = chromadb.PersistentClient(path="/home/john/git/annototal/annototal/db.chroma")
+        client = chromadb.PersistentClient(
+            path="/home/john/git/annototal/annototal/db.chroma"
+        )
         self.collection = client.get_collection(name="annotations")
 
     def __filter_distance(self, data, threshold):
-        uris = data['ids'][0]
-        distances = data['distances'][0]
-        #print(distances)
+        uris = data["ids"][0]
+        distances = data["distances"][0]
+        # print(distances)
         result = []
-        for uri,distance in zip(uris,distances):
+        for uri, distance in zip(uris, distances):
             if float(distance) <= threshold:
                 item = {"distance": distance, "uri": uri}
                 result.append(item)
-        unique_result = { each['distance'] : each for each in result }.values()
+        unique_result = {each["distance"]: each for each in result}.values()
         return list(unique_result)
 
+    def __annotation_page(self, items):
+        annotations = []
+        for item in items:
+            anno = requests.get(item["uri"], verify=False)
+            annotations.append(anno.json())
+        return {"id": request.url, "type": "AnnotationPage", "items": annotations}
 
-    def search(self, term: str, n: int, distance: float) -> tuple[int, list[str]]:
+    def search(self, term: str, n: int, distance: float) -> dict[str, object]:
         results = self.collection.query(
             query_texts=[term],
             n_results=n,
         )
-        #print(results)
-        result = self.__filter_distance(results, distance)
+        # print(results)
+        items = self.__filter_distance(results, distance)
+        result = self.__annotation_page(items)
         return result
+
 
 # class Context:
 #     pass
